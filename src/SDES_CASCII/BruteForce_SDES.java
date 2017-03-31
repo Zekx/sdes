c17df2423e6d3ac56d49121455a12c4b420dac0a
kcastil7 <kcas51@yahoo.com>
2017-03-31 00:58:38 -0700

    Added part 3

---
+++ src/SDES_CASCII/BruteForce_SDES.java
@@ -0,0 +1,101 @@
+package SDES_CASCII;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
+import sdes.SDES;
+
+public class BruteForce_SDES {
+
+
+	public static String byteToString(byte[] ar){
+		String byteStr = "";
+		for( byte a : ar){
+			byteStr = byteStr + String.valueOf(a);
+		}
+		return byteStr;
+	}
+
+	public static byte[] stringToByteArray(String str){
+		String[] arList = str.split("");
+		byte[] byteAr = new byte[str.length()];
+
+		for( int i = 0; i < arList.length - 1; i ++){
+			byteAr[i] = Byte.valueOf(arList[i]);
+		}
+		return byteAr;
+	}
+	
+	public static String intToBinary (int n, int numOfBits) {
+		   String binary = "";
+		   for(int i = 0; i < numOfBits; i++) {
+		      if(n%2 == 0){
+		    	  binary = "0" + binary;
+		      }
+		      else{
+		    	  binary = "1" + binary;
+		      }
+		      n=n/2;
+		   }
+
+		   return binary;
+		}
+
+	public static void main(String[] args) throws FileNotFoundException {
+		byte ciphertext[] = new byte[0];
+
+		try(BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Kevin\\Desktop\\CS4780\\sdes\\src\\resources\\msg1.txt"))) {
+			StringBuilder sb = new StringBuilder();
+			String line = br.readLine();
+
+			while (line != null) {
+				sb.append(line);
+				line = br.readLine();
+			}
+			String everything = sb.toString();
+			ciphertext = stringToByteArray(everything);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		String key = "";
+		for(int i = 0; i < Math.pow(2, 10); i++){
+			key = intToBinary(i,10);
+			byte[] plaintext = new byte[0];
+			byte[] temp = new byte[8];
+			byte bitarr[] = new byte[8];
+			int bcounter = 0;
+			for (int j = 0; j < ciphertext.length; j++){
+				if (bcounter <= 7){
+					bitarr[bcounter] = ciphertext[j];
+					bcounter++;
+				}
+				else {
+					temp = SDES.Decrypt(stringToByteArray(key), bitarr);
+					plaintext = SDES.appendArray(plaintext, temp);
+					bitarr[0] = ciphertext[j];
+					bcounter = 1;
+				}
+				if (j == ciphertext.length - 1){
+					temp = SDES.Decrypt(stringToByteArray(key), bitarr);
+					plaintext = SDES.appendArray(plaintext, temp);
+				}
+				
+			}
+			System.out.println("Key: " + key);
+			System.out.println("plaintext: " + CASCII.toString(plaintext));
+
+		}
+
+
+	}
+
+}
---
+++ src/SDES_CASCII/BruteForce_TSDES.java
@@ -0,0 +1,103 @@
+package SDES_CASCII;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import sdes.SDES;
+import triplesdes.TripleSDES;
+
+public class BruteForce_TSDES {
+
+	public static String byteToString(byte[] ar){
+		String byteStr = "";
+		for( byte a : ar){
+			byteStr = byteStr + String.valueOf(a);
+		}
+		return byteStr;
+	}
+
+	public static byte[] stringToByteArray(String str){
+		String[] arList = str.split("");
+		byte[] byteAr = new byte[str.length()];
+
+		for( int i = 0; i < arList.length - 1; i ++){
+			byteAr[i] = Byte.valueOf(arList[i]);
+		}
+		return byteAr;
+	}
+
+	public static String intToBinary (int n, int numOfBits) {
+		String binary = "";
+		for(int i = 0; i < numOfBits; i++) {
+			if(n%2 == 0){
+				binary = "0" + binary;
+			}
+			else{
+				binary = "1" + binary;
+			}
+			n=n/2;
+		}
+
+		return binary;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		byte ciphertext[] = new byte[0];
+
+		try(BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Kevin\\Desktop\\CS4780\\sdes\\src\\resources\\msg2.txt"))) {
+			StringBuilder sb = new StringBuilder();
+			String line = br.readLine();
+
+			while (line != null) {
+				sb.append(line);
+				line = br.readLine();
+			}
+			String everything = sb.toString();
+			ciphertext = stringToByteArray(everything);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		String key1 = "";
+		String key2 = "";
+		for(int i = 0; i < Math.pow(2, 10); i++){
+			key1 = intToBinary(i,10);
+			for(int k = 0; k < Math.pow(2, 10); k++){
+				key2 = intToBinary(k,10);
+				byte[] plaintext = new byte[0];
+				byte[] temp = new byte[8];
+				byte bitarr[] = new byte[8];
+				int bcounter = 0;
+				for (int j = 0; j < ciphertext.length; j++){
+					if (bcounter <= 7){
+						bitarr[bcounter] = ciphertext[j];
+						bcounter++;
+					}
+					else {
+						temp = TripleSDES.Decrypt(stringToByteArray(key1), stringToByteArray(key2), bitarr);
+						plaintext = SDES.appendArray(plaintext, temp);
+						bitarr[0] = ciphertext[j];
+						bcounter = 1;
+					}
+					if (j == ciphertext.length - 1){
+						temp = TripleSDES.Decrypt(stringToByteArray(key1), stringToByteArray(key2), bitarr);
+						plaintext = SDES.appendArray(plaintext, temp);
+					}
+
+				}
+				System.out.println("Key1: " + key1);
+				System.out.println("Key2: " + key2);
+				System.out.println("plaintext: " + CASCII.toString(plaintext));
+
+			}
+		}
+
+
+	}
+}
+
+
